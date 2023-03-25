3c5d86c6f1f98d5f68b81b6568d4cd8b70dea81f
Zachary Barrier <zbarrier@gmail.com>
2023-03-25 11:56:40 -0400

    Unit-3 Lesson-4

--- C#/ENG/src/Unit-3/lesson-4/dotnet/MovieStreaming/Actors/PlaybackActor.cs
+++ C#/ENG/src/Unit-3/lesson-4/dotnet/MovieStreaming/Actors/PlaybackActor.cs
@@ -5,25 +5,24 @@ using System.Threading.Tasks;
 using MovieStreaming.Messages;
 using Proto;
 
-namespace MovieStreaming.Actors
-{
-    public class PlaybackActor : IActor
-    {
-        public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
-        public Task ReceiveAsync(IContext context)
-        {
-            switch (context.Message)
-            {
-                case PlayMovieMessage msg:
-                    Console.WriteLine($"Received movie title {msg.MovieTitle}");
-                    Console.WriteLine($"Received user ID {msg.UserId}");
-                    break;
+namespace MovieStreaming.Actors;
 
-                case Stopped msg:
-                    Console.WriteLine("actor is Stopped");
-                    break;
-            }
-            return Actor.Done;
+public class PlaybackActor : IActor
+{
+    public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
+    public Task ReceiveAsync(IContext context)
+    {
+        switch (context.Message)
+        {
+            case PlayMovieMessage msg:
+                Console.WriteLine($"Received movie title {msg.MovieTitle}");
+                Console.WriteLine($"Received user ID {msg.UserId}");
+                break;
+
+            case Stopped msg:
+                Console.WriteLine("actor is Stopped");
+                break;
         }
+        return Task.CompletedTask;
     }
 }
