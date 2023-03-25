1a4c2de309bbac35a0e206e71c2d496199421f5d
Zachary Barrier <zbarrier@gmail.com>
2023-03-25 08:26:52 -0400

    updated Unit-2 Lesson-6

--- C#/ENG/src/Unit-2/lesson-6/dotnet/MovieStreaming/Program.cs
+++ C#/ENG/src/Unit-2/lesson-6/dotnet/MovieStreaming/Program.cs
@@ -2,40 +2,39 @@
 using System.Threading.Tasks;
 using Proto;
 
-namespace MovieStreaming
+namespace MovieStreaming;
+
+public class PlaybackActor : IActor
 {
-    public class PlaybackActor : IActor
+    public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
+    public Task ReceiveAsync(IContext context)
     {
-        public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
-        public Task ReceiveAsync(IContext context)
+        switch (context.Message)
         {
-            switch (context.Message)
-            {
-                case string movieTitle:
-                    Console.WriteLine($"Received movie title {movieTitle}");
-                    break;
-                case int userId:
-                    Console.WriteLine($"Received user ID {userId}");
-                    break;
-            }
-            return Actor.Done;
+            case string movieTitle:
+                Console.WriteLine($"Received movie title {movieTitle}");
+                break;
+            case int userId:
+                Console.WriteLine($"Received user ID {userId}");
+                break;
         }
+        return Task.CompletedTask;
     }
+}
 
-    class Program
+class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var system = new ActorSystem();
-            Console.WriteLine("Actor system created");
+        var system = new ActorSystem();
+        Console.WriteLine("Actor system created");
 
-            var props = Props.FromProducer(() => new PlaybackActor());
-            var pid = system.Root.Spawn(props);
+        var props = Props.FromProducer(() => new PlaybackActor());
+        var pid = system.Root.Spawn(props);
 
-            system.Root.Send(pid, "The Movie");
-            system.Root.Send(pid, 44);
+        system.Root.Send(pid, "The Movie");
+        system.Root.Send(pid, 44);
 
-            Console.ReadLine();
-        }
+        Console.ReadLine();
     }
 }
