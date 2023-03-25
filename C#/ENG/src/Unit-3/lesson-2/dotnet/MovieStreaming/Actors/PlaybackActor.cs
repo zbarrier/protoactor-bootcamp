af4972f7cc31385dd114e2ea92feacffa6fb62ec
Zachary Barrier <zbarrier@gmail.com>
2023-03-25 11:47:14 -0400

    Unit-3 and Lesson-2

--- C#/ENG/src/Unit-3/lesson-2/dotnet/MovieStreaming/Actors/PlaybackActor.cs
+++ C#/ENG/src/Unit-3/lesson-2/dotnet/MovieStreaming/Actors/PlaybackActor.cs
@@ -6,64 +6,63 @@ using System.Threading.Tasks;
 using MovieStreaming.Messages;
 using Proto;
 
-namespace MovieStreaming.Actors
+namespace MovieStreaming.Actors;
+
+public class PlaybackActor : IActor
 {
-    public class PlaybackActor : IActor
+    public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
+    public Task ReceiveAsync(IContext context)
     {
-        public PlaybackActor() => Console.WriteLine("Creating a PlaybackActor");
-        public Task ReceiveAsync(IContext context)
+        switch (context.Message)
         {
-            switch (context.Message)
-            {
-                case Started msg:
-                    ProcessStartedMessage(msg);
-                    break;
+            case Started msg:
+                ProcessStartedMessage(msg);
+                break;
 
-                case PlayMovieMessage msg:
-                    ProcessPlayMovieMessage(msg);
-                    break;
+            case PlayMovieMessage msg:
+                ProcessPlayMovieMessage(msg);
+                break;
 
-                case Recoverable msg:
-                    ProcessRecoverableMessage(context, msg);
-                    break;
+            case Recoverable msg:
+                ProcessRecoverableMessage(context, msg);
+                break;
 
-                case Stopping msg:
-                    ProcessStoppingMessage(msg);
-                    break;
-            }
-            return Actor.Done;
+            case Stopping msg:
+                ProcessStoppingMessage(msg);
+                break;
+        }
+        return Task.CompletedTask;
+    }
+
+    private void ProcessStartedMessage(Started msg)
+    {
+        ColorConsole.WriteLineGreen("PlaybackActor Started");
+    }
+
+    private void ProcessPlayMovieMessage(PlayMovieMessage msg)
+    {
+        ColorConsole.WriteLineYellow($"PlayMovieMessage {msg.MovieTitle} for user {msg.UserId}");
+    }
+
+    private void ProcessRecoverableMessage(IContext context, Recoverable msg)
+    {
+        PID child;
+
+        if (context.Children == null || context.Children.Count == 0)
+        {
+            var props = Props.FromProducer(() => new ChildActor());
+            child = context.Spawn(props);
+        }
+        else
+        {
+            child = context.Children.First();
         }
 
-        private void ProcessStartedMessage(Started msg)
-        {
-            ColorConsole.WriteLineGreen("PlaybackActor Started");
-        }
+        context.Forward(child);
+    }
 
-        private void ProcessPlayMovieMessage(PlayMovieMessage msg)
-        {
-            ColorConsole.WriteLineYellow($"PlayMovieMessage {msg.MovieTitle} for user {msg.UserId}");
-        }
-
-        private void ProcessRecoverableMessage(IContext context, Recoverable msg)
-        {
-            PID child;
-
-            if (context.Children == null || context.Children.Count == 0)
-            {
-                var props = Props.FromProducer(() => new ChildActor());
-                child = context.Spawn(props);
-            }
-            else
-            {
-                child = context.Children.First();
-            }
-
-            context.Forward(child);
-        }
-
-        private void ProcessStoppingMessage(Stopping msg)
-        {
-            ColorConsole.WriteLineGreen("PlaybackActor Stopping");
-        }
+    private void ProcessStoppingMessage(Stopping msg)
+    {
+        ColorConsole.WriteLineGreen("PlaybackActor Stopping");
     }
 }
--- C#/ENG/src/Unit-3/lesson-2/dotnet/MovieStreaming/ColorConsole.cs
+++ C#/ENG/src/Unit-3/lesson-2/dotnet/MovieStreaming/ColorConsole.cs
@@ -2,30 +2,20 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace MovieStreaming
+namespace MovieStreaming;
+
+public static class ColorConsole
 {
-    public static class ColorConsole
+    public static void WriteLineGreen(string message) => WriteLineColor(message, ConsoleColor.Green);
+    public static void WriteLineYellow(string message) => WriteLineColor(message, ConsoleColor.Yellow);
+
+    static void WriteLineColor(string message, ConsoleColor color)
     {
-        public static void WriteLineGreen(string message)
-        {
-            var beforeColor = Console.ForegroundColor;
+        var originalColor = Console.ForegroundColor;
 
-            Console.ForegroundColor = ConsoleColor.Green;
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
 
-            Console.WriteLine(message);
-
-            Console.ForegroundColor = beforeColor;
-        }
-
-        public static void WriteLineYellow(string message)
-        {
-            var beforeColor = Console.ForegroundColor;
-
-            Console.ForegroundColor = ConsoleColor.Yellow;
-
-            Console.WriteLine(message);
-
-            Console.ForegroundColor = beforeColor;
-        }
+        Console.ForegroundColor = originalColor;
     }
 }
